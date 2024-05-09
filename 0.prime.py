2bebfceae2e20dd374748e36af3fb6ab9aba520f
SaiRameshkumar14 <saiddiwaker141102@gmail.com>
2024-05-09 12:37:44 +0530

    tcs

---
+++ 0.prime.py
@@ -0,0 +1,36 @@
+
+n = int(input())
+flag = 0
+
+for i in range(2,n):
+    if n % i == 0:
+        flag = 1
+        break
+
+if flag == 1:
+    print("Not Prime")
+else:  
+    print("Prime")
+
+
+# def is_prime(num):
+#     if num < 2:
+#         return False
+#     for i in range(2, int(num ** 0.5) + 1):
+#         if num % i == 0:
+#             return False
+#     return True
+
+# def find_primes_between(num1, num2):
+#     prime_numbers = []
+#     for num in range(num1, num2):
+#         if is_prime(num):
+#             prime_numbers.append(num)
+#     return prime_numbers
+
+# # Example usage:
+# num1 = 1
+# num2 = 100
+# prime_numbers_between_1_and_100 = find_primes_between(num1, num2)
+# print("Prime numbers between 1 and 100:", prime_numbers_between_1_and_100)
+

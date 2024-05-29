8509ebb04810903d9cae03bb59235689db7d325e
SaiRameshkumar14 <saiddiwaker141102@gmail.com>
2024-05-29 16:50:28 +0530

    ztm

--- Udemy/ZTM-Udemy/find_duplicates.py
+++ Udemy/ZTM-Udemy/find_duplicates.py
@@ -1,11 +1,16 @@
 a = [1,2,2,3,4,2,3,4,5,6,7,8]
 b = []
 
+#taking element in a
 for i in a:
+    #counting the element in a , if it more than one.
     if a.count(i) > 1:
-        b.append(i)
+        #checking if the element is already in the b
+        if i not in b:
+            #appending the element in b
+            b.append(i)
 
+print (b)    #[2, 3, 4]
 
-print(list(set(b)))
+print(list(set(b)))   #[2, 3, 4]
 
-#[2, 3, 4]
